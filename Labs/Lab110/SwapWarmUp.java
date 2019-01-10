8c30766c4f65fa3b809c1cefd10f2b38eea1413b
Nick Gonzales <42616190+797985@users.noreply.github.com>
2019-01-10 10:30:43 -0800

    Lab110, almost done except I need to do selection sort

---
+++ Labs/Lab110/SwapWarmUp.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class SwapWarmUp here.
+ *
+ * @author (Nick Gonzales)
+ * @version (1/09/19)
+ */
+
+public class SwapWarmUp
+{
+    public static void swap(int a, int b){
+        a = 15;
+        b =3;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        
+    }
+}
