c45421dd415fc24cef9840108ecdfc87305e5968
Nick Gonzales <42616190+797985@users.noreply.github.com>
2019-01-23 10:21:16 -0800

    did some of lab118 mergeSort

--- Labs/Lab118MergeSort/MergeSort.java
+++ Labs/Lab118MergeSort/MergeSort.java
@@ -7,20 +7,23 @@
  */
 public class MergeSort
 {
+   
+    
     public static void mergeSort(int[] a, int n)
     {
+        int array[];
         
         if (n<2) {return;}
-        int mid = n/2;
-        int[] l = new int[mid];
-        int[] r = new int[n - mid];
-        for (int i=0; i<mid; i++){
-            l[i] = a[i];
-        }
-        for (int i = mid; i < n; i++){
-            r[i-mid] = a[i];
+            int mid = n/2;
+            int[] l = new int[mid];
+            int[] r = new int[n - mid];
+            for (int i=0; i<mid; i++){
+                l[i] = a[i];
+            }
+            for (int i = mid; i < n; i++){
+                r[i-mid] = a[i];
             
-        }
+            }
         mergeSort(l, mid);
         mergeSort(r, n-mid);
         //helper function
@@ -28,15 +31,15 @@ public class MergeSort
     }
     public static void merge(int[] a, int[] l, int[] r, int left, int right){
         int i = 0, j = 0, k = 0;
-    while (i < left && j < right) {
-        if (l[i] <= r[j]) {
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
             a[k++] = l[i++];
-        }else {
+            }else {
             a[k++] = r[j++];
+            }
         }
-    }
-    while (i < left) { a[k++] = l[i++];}
-    while (j < right) {a[k++] = r[j++];}
+        while (i < left) { a[k++] = l[i++];}
+        while (j < right) {a[k++] = r[j++];}
 
     }
     public void printArray(int arr[]) 
@@ -45,14 +48,15 @@ public class MergeSort
         for (int l=0; l<r; ++l) 
             System.out.print(arr[l]+" "); 
         System.out.println(); 
-    } 
+    }
+    //organizeing my student number and random stuNums that I made up
     public static void main(String args[]) 
     { 
         MergeSort object = new MergeSort(); 
-        int arr[] = {35,47,15,31,9}; 
-        object.mergeSort(arr); 
+        int array[] = {797985, 797845, 795784, 799103, 796385, 787281}; 
+        object.mergeSort(array); 
         System.out.println("Sorted array"); 
-        object.printArray(arr); 
+        object.printArray(array); 
     } 
 }
 
---
+++ Labs/Lab118MergeSort/StudentRunner.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class StudentRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StudentRunner
+{
+    // instance variables - replace the example below with your own
+    public static void main(){
+        Student2 s2 = new Student2();
+        System.out.println(s2.getStuNumber());
+        
+    }
+}
