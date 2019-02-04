ab5c73647c0e41fbfc82cb5bfaf288be16f55f1f
Nick Gonzales <42616190+797985@users.noreply.github.com>
2019-02-04 10:28:32 -0800

    Did lab31

---
+++ Labs/Lab131/CanvasComponent.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class CanvasComponent here.
+ *
+ * @author (Nick Gonzales)
+ * @version (2/4/19)
+ */
+import java.awt.*;
+import javax.swing.*;
+public class CanvasComponent
+{
+    // instance variables - replace the example below with your own
+    private int xPosition;
+    private int yPosition;    
+    private int width;
+    private int height;
+    /**
+     * Constructor for objects of class CanvasComponent
+     */
+    public CanvasComponent(int width, int height)
+    {
+        
+        
+    }
+    
+}
---
+++ Labs/Lab131/MySwingApplication.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class MySwingApplication here.
+ *
+ * @author (Nick Gonzales)
+ * @version (2/4/19)
+ */
+
+import javax.swing.*;
+public class MySwingApplication implements Runnable
+{
+    JFrame jFrame; //first class name
+    public void run(){
+         jFrame = new JFrame("Window");
+         jFrame.setSize(400, 400);
+         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         CanvasComponent omponent = new CanvasComponent(100, 100);
+         jFrame.setVisible(true);
+         
+    }
+    public static void main(){
+        MySwingApplication mySwingApplicationSwing = new MySwingApplication();
+        javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
+    }
+    
+}
