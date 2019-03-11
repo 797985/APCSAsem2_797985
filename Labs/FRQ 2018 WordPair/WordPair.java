11e9e1253bdadaa52803470baf74ec213e199e89
Nick Gonzales <42616190+797985@users.noreply.github.com>
2019-03-11 10:28:48 -0700

    did the FRQ

---
+++ Labs/FRQ 2018 WordPair/WordPair.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class WordPair here.
+ *
+ * @author (Nick Gonzales)
+ * @version (3/11/19)
+ */
+public class WordPair
+{
+    private int x;
+    private String firstWord;
+    private String secondWord;
+    public WordPair(String first, String second){
+        firstWord = first;
+        secondWord = second;
+    }
+    public String getFirst(){
+        return firstWord;
+    }
+    public String getSecond(){
+        return secondWord;
+    }
+}
+
