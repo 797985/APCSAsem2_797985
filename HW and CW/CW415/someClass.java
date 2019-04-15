0104a7fae058753d64a3ea3a6d695f5438ba3aca
797985 <797985@seq.org>
2019-04-15 12:14:16 -0700

    CW415

---
+++ HW and CW/CW415/someClass.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class someClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.ArrayList;
+public class someClass
+{
+    public void sortStrings(ArrayList<String> names){
+        String[] name = {"Nick, Reis, Chris, Connor, Freddy"};
+        int count = 0;
+        String sortStrings[] = (name, name.length);
+        
+        
+    }
+}
