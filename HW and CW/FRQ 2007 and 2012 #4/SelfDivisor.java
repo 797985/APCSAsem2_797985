8758e470188f130594bcf43dcf1f135514d28e60
797985 <797985@seq.org>
2019-04-29 11:51:32 -0700

    stuff

---
+++ HW and CW/FRQ 2007 and 2012 #4/SelfDivisor.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class SelfDivisor here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SelfDivisor
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    
+    public static boolean isSelfDivisor(int number){
+        
+        if(number % number = 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static int[] firstNumSelfDivisor(int start, int num){
+        
+    }
+    /**
+     * Constructor for objects of class SelfDivisor
+     */
+    public SelfDivisor()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
